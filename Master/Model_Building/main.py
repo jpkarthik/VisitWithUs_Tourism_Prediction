e12f9b906cc5d9069cab7fd04723d28f55c4b45c
jpkarthik <jpkaimlgl@gmail.com>
2025-08-09 19:43:18 +0530

    Fixes typo in main

--- Master/Model_Building/main.py
+++ Master/Model_Building/main.py
@@ -10,8 +10,8 @@ print(f'Base path {base_path}')
 sys.path.append(os.path.join(base_path,'Model_Building'))
 print(f'System Path {sys.path.append(os.path.join(base_path,'Model_Building'))}')
 from DataRegistration import DataRegistration
-from DataPrepration import DataPrepration
-from BuildingModels import BuildingModels
+# from DataPrepration import DataPrepration
+# from BuildingModels import BuildingModels
 
 data_dir = os.path.join(base_path, 'Data')
 model_dir = os.path.join(base_path,'Model_Dump_JOBLIB')
@@ -28,34 +28,3 @@ if job == 'register':
   data_reg = DataRegistration(base_path, hf_token)
   if not data_reg.To_run_Pipeline():
     sys.exit(1)
-
-
-elif job == 'prepare':
-  data_prep = DataPrepration(base_path, hf_token)
-  df_dataset = data_prep.LoadDatasetFromHF()
-  if df_dataset is not None:
-    df_train, df_test = data_prep.TrainTestSplit()
-    if df_train is not None and df_test is not None:
-      df_train_cleaned = data_prep.CleanData(df_train)
-      df_test_cleaned = data_prep.CleanData(df_test)
-      data_prep.UploadIntoHF(df_train_cleaned,data_dir,'train.csv')
-      data_prep.UploadIntoHF(df_test_cleaned,data_dir,'test.csv')
-
-elif job == 'build':
-  model_build = BuildingModels(base_path, hf_token)
-  model_build.Load_data_from_HF()
-  model_build.Preprocessing_dataset()
-  models = model_build.Building_Models()
-  if models:
-    metrics = model_build.Model_Evaluation()
-    model_build.Register_BestModel_HF()
-  else:
-    print("Unable to build the models")
-
-
-
-
-  
-
-
-  
